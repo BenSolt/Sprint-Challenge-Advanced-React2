df863d89b0bc16bfb2819680d82f896757fb2277
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-01 15:07:09 -0400

    set up axios call

---
+++ client/src/components/NavbarSlider.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useDarkMode } from "../hooks/useDarkMode";
+import { useDarkMode2 } from "../hooks/useDarkModeRed";
+
+const NavbarSlider = () => {
+    const [darkMode, setDarkMode] = useDarkMode(false);
+    const toggleMode = e => {
+      e.preventDefault();
+      setDarkMode(!darkMode);
+  
+    };
+
+      // DarkMode Red
+  const [darkMode2, setDarkMode2] = useDarkMode2(false);
+  const toggleMode2 = e => {
+    e.preventDefault();
+    setDarkMode2(!darkMode2);
+    
+  };
+  
+    return (
+      <nav className="navbar">
+        <h2>Dark</h2>
+        <div className="dark-mode__toggle">
+            <div
+            onClick={toggleMode}
+            className={darkMode ? 'toggle toggled' : 'toggle'}
+            />
+        </div>
+
+        {/* Dark Mode Red */}
+        <h2>Red</h2>
+        <div className="dark-mode__toggle">
+          <div
+          onClick={toggleMode2}
+          className={darkMode2 ? 'toggle toggled' : 'toggle'}
+          />
+        </div>
+
+  </nav>
+
+    )
+
+};
+
+export default NavbarSlider;
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+
+class PlayerCard extends React.Component {
+
+    constructor(){
+      super()
+      this.state = {
+     players: []
+      };
+    }
+
+    // componentDidMount() {
+    //     console.log("first render(mounting)");
+    //     axios
+    //     .get("http://localhost:5000/api/players")
+    //     .then(res => console.log(res.data))
+
+        // .then(res => this.setState({ players: res.data }))
+        //.catch(err => console.log("error"));
+     // }
+
+      render() {
+
+        return (
+      
+          <div>
+      
+          {this.state.players.map(player => 
+          
+          <div className="card1">
+      
+          <h2>Name: {player.name}</h2>
+          <h2>Country: {player.country}</h2>
+          <h2>Times Searched: {player.searches}</h2> 
+      
+          </div>
+          
+          )}
+      
+          </div>   
+       
+        )
+      
+      }
+      }
+            
+        export default PlayerCard;
